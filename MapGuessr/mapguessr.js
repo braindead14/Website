b5b3884e92d3e37c82c5e058dc78aa64e8b36604
braindead14 <129587858+braindead14@users.noreply.github.com>
2024-03-11 23:30:08 +0100

    what hell have i gotten myself into
    doesn't even work

--- MapGuessr/mapguessr.js
+++ MapGuessr/mapguessr.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function draw() {
+    const canvas = document.getElementById("map");
+    const ctx = canvas.getContext("2d");
+
+    fetch('country_shapes_simplified.json')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error fetching JSON:', error));
+
+    ctx.beginPath();
+    ctx.moveTo(25, 25);
+    ctx.lineTo(105, 25);
+    ctx.lineTo(25, 105);
+    ctx.fill();
+}
+
+window.addEventListener("load", draw);
