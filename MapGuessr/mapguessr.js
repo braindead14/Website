d36f6a5b9897e12f140982eabdb89162c53f2d7c
braindead14 <129587858+braindead14@users.noreply.github.com>
2024-03-14 19:23:33 +0100

    added GAMEPLAY

--- MapGuessr/mapguessr.js
+++ MapGuessr/mapguessr.js
@@ -4,74 +4,76 @@ function draw() {
     const canvas = document.getElementById("map");
     const ctx = canvas.getContext("2d");
 
-    /*
-    fetch('country_shapes_simplified.json')
-        .then(response => response.json())
-        .then(data => logData(data))
-        .catch(error => console.error('Error fetching JSON:', error));
-    */
+    const targetSize = window.innerWidth * 0.2;
+    const randomCountry = Math.floor(Math.random() * 251);       // Random number between [0, 251]
+    let countryName;
+
+    ctx.canvas.width  = targetSize;
+    ctx.canvas.height = ctx.canvas.width;
+
+    // ctx.strokeStyle = "#ffffff";
+    ctx.fillStyle = "#21424c";
     
     fetch('country_shapes.json')                                        // Length: 252 (=> 0 - 251)
         .then(response => response.json())
-        .then(data => logData(ctx, data))
+        .then(data => {
+            handleData(ctx, data, targetSize, randomCountry); 
+            countryName = data[randomCountry]["cntry_name"];
+
+            const textInputGuess = document.getElementById("guess");
+            const skipButton = document.getElementById("skipButton");
+            textInputGuess.countryName = countryName;
+            skipButton.countryName = countryName;
+        })
         .catch(error => console.error('Error fetching JSON:', error));
-
     
-     
+    const textInputGuess = document.getElementById("guess");
+    const skipButton = document.getElementById("skipButton");
+    const nextButton = document.getElementById("nextButton");
+
+    textInputGuess.addEventListener("keypress", guessHandler);
+    skipButton.addEventListener("click", skipButtonHandler);
+    nextButton.addEventListener("click", nextButtonHandler);
 }
 
-function logData(ctx, data) {
-    const len = Object.keys(data).length;
-    const code = 1;
+function handleData(ctx, data, targetSize, code) {
+    const countryNameDiv = document.getElementById("countryName");
 
-    // Lets plot Belgium
-    console.log(data[code]);
-    plotShape(ctx, data[code]["geo_shape"]["geometry"]["coordinates"][0]);
-}
-
-function plotShape(ctx, points) {
-    // TODO: handle multipolygons!! and preserve aspact ratio
-
-    const len = points.length;
-    ctx.strokeStyle = "#ffffff";
-    ctx.fillStyle = "#21424c";
-    
-    let min_lon = points[0][0];
-    let max_lon = points[0][0];
-    let min_lat = points[0][1];
-    let max_lat = points[0][1];
-
-    // Get min and max
-    for(let i = 0; i < points.length; i++){
-        if(points[i][0] < min_lon) {
-            min_lon = points[i][0];
-        }
-
-        if(points[i][1] < min_lat) {
-            min_lat = points[i][1];
-        }
-
-        if(points[i][0] > max_lon) {
-            max_lon = points[i][0];
-        }
-
-        if(points[i][1] > max_lat) {
-            max_lat = points[i][1];
-        }
+    if (data[code]["geo_shape"]["geometry"]["type"] == "Polygon") {
+        plotPolygon(ctx, data[code]["geo_shape"]["geometry"]["coordinates"][0], targetSize);
+    } else {
+        plotMultipolygon(ctx, data[code]["geo_shape"]["geometry"]["coordinates"], targetSize);
     }
+    // countryNameDiv.innerText = data[code]["cntry_name"];
+}
 
-    console.log(min_lon);
-    console.log(min_lat);
-    console.log(max_lon);
-    console.log(max_lat);
+function plotPolygon(ctx, points, targetSize) {
+    const len = points.length;
+
+    const minPoints = getMinOfPoints(points);
+    const maxPoints = getMaxOfPoints(points);
+    
+    const min_lon = minPoints[0];
+    const max_lon = maxPoints[0];
+    const min_lat = minPoints[1];
+    const max_lat = maxPoints[1];
+
+    const deltaLat = max_lat - min_lat;
+    const deltaLon = max_lon - min_lon;
+
+    const aspectRatio = deltaLon / deltaLat;          // lon = x, lat = y
+
+    const scaleFactor = computeScaleFactor(deltaLat, deltaLon, targetSize);
 
     // Rescale
-    for(let i = 0; i < points.length; i++){
-        points[i][0] = 150*(points[i][0] - min_lon)/(max_lon - min_lon);
-        points[i][1] = 150 - 150*(points[i][1] - min_lat)/(max_lat - min_lat);
+    for (let i = 0; i < points.length; i++) {
+        const lon = points[i][0];
+        const lat = points[i][1];
+    
+        // Apply the scaling factor
+        points[i][0] = (lon - min_lon) * scaleFactor;
+        points[i][1] = targetSize - (lat - min_lat) * scaleFactor;
     }
-
-    console.log(points);
 
     ctx.beginPath();
     ctx.moveTo(points[0][0], points[0][1]);
@@ -81,6 +83,151 @@ function plotShape(ctx, points) {
     }
 
     ctx.fill();  
+}
+
+function plotMultipolygon(ctx, points, targetSize) {
+    let min_lon_arr = [];
+    let max_lon_arr = [];
+    let min_lat_arr = [];
+    let max_lat_arr = [];
+
+    for (let i = 0; i < points.length; i++) {
+        const minPoints = getMinOfPoints(points[i][0]);
+        const maxPoints = getMaxOfPoints(points[i][0]);
+
+        min_lon_arr.push(minPoints[0]);
+        max_lon_arr.push(maxPoints[0]);
+        min_lat_arr.push(minPoints[1]);
+        max_lat_arr.push(maxPoints[1]);
+    }
+
+    const min_lon = Math.min(...min_lon_arr);
+    const max_lon = Math.max(...max_lon_arr);
+    const min_lat = Math.min(...min_lat_arr);
+    const max_lat = Math.max(...max_lat_arr);
+
+    const deltaLat = max_lat - min_lat;
+    const deltaLon = max_lon - min_lon;
+
+    const aspectRatio = deltaLon / deltaLat;          // lon = x, lat = y
+
+    const scaleFactor = computeScaleFactor(deltaLat, deltaLon, targetSize);
+
+    // Rescale
+    for (let j = 0; j < points.length; j++) {
+        for (let i = 0; i < points[j][0].length; i++) {
+            const lon = points[j][0][i][0];
+            const lat = points[j][0][i][1];
+        
+            // Apply the scaling factor
+            points[j][0][i][0] = (lon - min_lon) * scaleFactor;
+            points[j][0][i][1] = targetSize - (lat - min_lat) * scaleFactor;
+        }
+    }
+
+    for (let j = 0; j < points.length; j++) {
+        ctx.beginPath();
+        ctx.moveTo(points[j][0][0][0], points[j][0][0][1]);
+
+        for(let k = 0; k < points[j][0].length; k++) {
+            ctx.lineTo(points[j][0][k][0], points[j][0][k][1]);
+        }
+
+        ctx.fill(); 
+    }
+}
+
+function getMaxOfPoints(arr) {
+    let max_lon = arr[0][0];
+    let max_lat = arr[0][1];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i][0] > max_lon) {
+            max_lon = arr[i][0];
+        }
+
+        if(arr[i][1] > max_lat) {
+            max_lat = arr[i][1];
+        }
+    }
+    return [max_lon, max_lat];
+}
+
+function getMinOfPoints(arr) {
+    let min_lon = arr[0][0];
+    let min_lat = arr[0][1];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i][0] < min_lon) {
+            min_lon = arr[i][0];
+        }
+
+        if(arr[i][1] < min_lat) {
+            min_lat = arr[i][1];
+        }
+    }
+    return [min_lon, min_lat];
+}
+
+function computeScaleFactor(deltaLat, deltaLon, targetSize) {
+    const aspectRatio = deltaLon / deltaLat;          // lon = x, lat = y
+
+    let scaleFactor;
+    if (aspectRatio > 1) {
+        // Landscape shape (wider than tall)
+        scaleFactor = targetSize / deltaLon;
+    } else {
+        // Portrait shape (taller than wide)
+        scaleFactor = targetSize / deltaLat;
+    }
+
+    return scaleFactor;
+}
+
+function skipButtonHandler(evt) {
+    const countryName = evt.currentTarget.countryName;
+
+    // Reset input field text
+    const textInputGuess = document.getElementById("guess");
+    textInputGuess.value = "";
+
+    // Show right answer
+    const guessResultDiv = document.getElementById("guessResult");
+    guessResultDiv.innerText = "The country was " + countryName + "!";
+
+    // Redraw
+    draw();
+}
+
+function nextButtonHandler() {
+    // Reset input field text
+    const textInputGuess = document.getElementById("guess");
+    textInputGuess.value = "";
+
+    // Reset guess text field
+    document.getElementById("guessResult").innerText = "";
+
+    // Reset visibility
+    document.getElementById("nextButton").style.visibility = "hidden";
+
+    // Redraw
+    draw();
+}
+
+function guessHandler(evt) {
+    if (evt.key === 'Enter') {
+        const guessResultDiv = document.getElementById("guessResult");
+        const correctCountry = evt.currentTarget.countryName;
+        const textInputGuess = document.getElementById("guess");
+        // guessResultDiv.innerText = evt.currentTarget.countryName;
+
+        const guess = textInputGuess.value;
+
+        if (!guess.localeCompare(correctCountry, undefined, { sensitivity: 'accent' })) {
+            guessResultDiv.innerText = "Correct! You get an extra brownie point";
+            document.getElementById("nextButton").style.visibility = "visible";
+        } else {
+            guessResultDiv.innerText = "Wrong answer, try again";
+        }
+    }
 }
 
 window.addEventListener("load", draw);
